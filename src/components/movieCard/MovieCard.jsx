c8c2103b3aef4a75b228c527f05c2fa0919e2524
Hitesh Malik <malik.hitu90@gmail.com>
2023-09-02 21:15:01 +0530

    frammer used for animation on card

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -9,34 +9,50 @@ import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
 
-const MovieCard = ({ data, fromSearch, mediaType }) => {
+import { motion } from "framer-motion";
+
+
+const   MovieCard = ({ data, fromSearch, mediaType }) => {
     const { url } = useSelector((state) => state.home);
     const navigate = useNavigate();
     const posterUrl = data.poster_path
         ? url.poster + data.poster_path
         : PosterFallback;
     return (
+
         <div
             className="movieCard"
             onClick={() =>
                 navigate(`/${data.media_type || mediaType}/${data.id}`)
             }
         >
-            <div className="posterBlock">
-                <Img className="posterImg" src={posterUrl} />
-                {!fromSearch && (
-                    <React.Fragment>
-                        <CircleRating rating={data.vote_average.toFixed(1)} />
-                        <Genres data={data.genre_ids.slice(0, 2)} />
-                    </React.Fragment>
-                )}
-            </div>
-            <div className="textBlock">
-                <span className="title">{data.title || data.name}</span>
-                <span className="date">
-                    {dayjs(data.release_date).format("MMM D, YYYY")}
-                </span>
-            </div>
+            <motion.div
+                className="box"
+                initial={{ opacity: 0, scale:0.2 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 1,
+                    delay: 0.5,
+                    ease: [0, 0.71, 0.2, 1.01]
+                }}
+            >
+                <div className="posterBlock">
+                    <Img className="posterImg" src={posterUrl} />
+                    {!fromSearch && (
+                        <React.Fragment>
+                            <CircleRating rating={data.vote_average.toFixed(1)} />
+                            <Genres data={data.genre_ids.slice(0, 2)} />
+                        </React.Fragment>
+                    )}
+                </div>
+                <div className="textBlock">
+                    <span className="title">{data.title || data.name}</span>
+                    <span className="date">
+                        {dayjs(data.release_date).format("MMM D, YYYY")}
+                    </span>
+                </div>
+            </motion.div>
+
         </div>
     );
 };
